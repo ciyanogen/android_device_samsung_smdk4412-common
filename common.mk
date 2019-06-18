379472c2e8d1a38e8784950bfdd61a83ac27165d
Seden Ecem <ttbpccorporation@outlook.com>
2019-06-18 19:42:02 +0000

    Changes for my custom lineage-15.1 build * Add OpenGL Renderer to system.prop * Rename go_galaxys2-common.mk to go_smdk4412-common.mk
    Change-Id: I74b4024286a51b4e5be5c80cff3b93b177a372f8
    Signed-off-by: Seden Ecem <ttbpccorporation@outlook.com>

--- common.mk
+++ common.mk
@@ -249,8 +249,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-threads=1 \
     dalvik.vm.image-dex2oat-threads=1
 
-# Build with specific settings for Galaxys2-common
-$(call inherit-product, $(LOCAL_PATH)/go_galaxys2-common.mk)
+# Build with specific settings for smdk4412-common
+$(call inherit-product, $(LOCAL_PATH)/go_smdk4412-common.mk)
 
 # Include debugging props
 $(call inherit-product, device/samsung/smdk4412-common/system_prop_debug.mk)
